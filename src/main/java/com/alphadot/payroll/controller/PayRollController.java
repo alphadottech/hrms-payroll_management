4cf943f92aa305687b9b63d346aae31aa5b7061b
DhananjayBobde <113025710+DhananjayBobde@users.noreply.github.com>
2023-03-28 12:17:19 +0530

    check in

--- src/main/java/com/alphadot/payroll/controller/PayRollController.java
+++ src/main/java/com/alphadot/payroll/controller/PayRollController.java
@@ -1,18 +1,17 @@
 package com.alphadot.payroll.controller;
 
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.ParseException;
 
-import javax.annotation.security.RolesAllowed;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,23 +27,28 @@ public class PayRollController {
 	  
 	@Autowired
 	private PayRollService payRollService;
-
 	
 
+
+
 	@GetMapping("/slip")
-	public ResponseEntity<PaySlip> payrollCreate(@RequestParam("empId") int empId, @RequestParam("month") String month, @RequestParam("year") String year, HttpServletRequest request) throws ParseException, IOException {
+	public ResponseEntity<PaySlip> payrollCreate(@RequestParam("empId") int empId, @RequestParam("month") String month, @RequestParam("year") String year,@RequestParam("adhoc") int adhoc, HttpServletRequest request) throws ParseException, IOException, InvalidFormatException, SQLException {
 		  LOGGER.info("API Call From IP: " + request.getRemoteHost());
-		return ResponseEntity.ok(payRollService.createPaySlip(empId,month,year));
+		return ResponseEntity.ok(payRollService.createPaySlip(empId,month,year,adhoc));
 	}
 	
 
-	@GetMapping("/genPay")
-	public void generatePaySlip() throws IOException {
+	@GetMapping("/genPayAll")
+	public void generatePaySlip() throws IOException, ParseException {
 		
 		payRollService.generatePaySlip();
 		
 	
 	}
 	
+
+	
+	
+
   
 }
---
+++ src/main/java/com/alphadot/payroll/model/EmailDetails.java
@@ -0,0 +1,52 @@
+package com.alphadot.payroll.model;
+
+public class EmailDetails {
+	 
+    // Class data members
+    private String recipient;
+    private String msgBody;
+    private String subject;
+    private String attachment;
+	public String getRecipient() {
+		return recipient;
+	}
+	public void setRecipient(String recipient) {
+		this.recipient = recipient;
+	}
+	public String getMsgBody() {
+		return msgBody;
+	}
+	public void setMsgBody(String msgBody) {
+		this.msgBody = msgBody;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getAttachment() {
+		return attachment;
+	}
+	public void setAttachment(String attachment) {
+		this.attachment = attachment;
+	}
+	public EmailDetails(String recipient, String msgBody, String subject, String attachment) {
+		super();
+		this.recipient = recipient;
+		this.msgBody = msgBody;
+		this.subject = subject;
+		this.attachment = attachment;
+	}
+	public EmailDetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public String toString() {
+		return "EmailDetails [recipient=" + recipient + ", msgBody=" + msgBody + ", subject=" + subject
+				+ ", attachment=" + attachment + "]";
+	}
+    
+    
+}
