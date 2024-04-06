40d8910c4b6715e9a27446843624fe78f8489dea
ankitdeshmu <ankitdeshmukh.adt@gmail.com>
2024-04-06 15:34:47 +0530

    Leave request issue changes in payroll service

--- src/main/java/com/adt/payroll/service/LeaveRequestServiceImpl.java
+++ src/main/java/com/adt/payroll/service/LeaveRequestServiceImpl.java
@@ -49,7 +49,7 @@ public class LeaveRequestServiceImpl implements LeaveRequestService {
 
 	@Value("${spring.mail.username}")
 	private String sender;
-
+    @Autowired
 	private ApplicationEventPublisher applicationEventPublisher;
 
 	public LeaveRequestServiceImpl() {
