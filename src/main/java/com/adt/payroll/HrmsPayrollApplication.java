104363ea65a58cd6fbdd0f2401993f51d8784b35
Nisha singh <nishasingh.adt@gmail.com>
2024-07-30 17:44:54 +0530

    annotation removed

--- src/main/java/com/adt/payroll/HrmsPayrollApplication.java
+++ src/main/java/com/adt/payroll/HrmsPayrollApplication.java
@@ -1,16 +1,15 @@
 package com.adt.payroll;
 
-import com.adt.payroll.service.TableDataExtractor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.adt.payroll.service.TableDataExtractor;
+
 @SpringBootApplication
 @EnableScheduling
-@EnableAsync
 public class HrmsPayrollApplication {
 
 	public static void main(String[] args) {
